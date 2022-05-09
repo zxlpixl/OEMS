bbf36580d49873b45237f1eea56c4a47403840ad
zxlpixl <jasonlzx1306@gmail.com>
2022-05-09 15:38:36 +0800

    added quit,clear,main menu functions

--- Main.py
+++ Main.py
@@ -1,1 +1,26 @@
+import time
+import os
 
+
+#clear command
+def clear():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+#main menu function
+def main_menu():
+    clear()
+    menu = '''
+Welcome to OEM, The Online Event Management System!
+What would you like to do?
+1. Log In
+2. Register An Account
+3. Event Information
+4. Exit'''
+
+    print(menu)
+
+main_menu()
